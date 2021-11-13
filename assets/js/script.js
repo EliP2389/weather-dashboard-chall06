fc3687cb3408e72fb344e7e35f4ae13a9c12af13
Eli Pagan <mbk8689@gmail.com>
2021-11-13 10:51:51 -0500

    initial html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-var listedCitiesEl = document.querySelector("#listed-cities");
 var citySearchEl = document.querySelector("#citysearch");
-var weatherEl = document.querySelector("#weather");
-var forcastEl = document.querySelector("forcast");
+var weatherEl = document.querySelector("#current-weather");
+var fiveDayEl = document.querySelector("fiveday-forcast");
+
+var apiKey = "";
+
+var weatherToday = function(){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={4e7c7b8b1310e93443a71c3595bacafe}"
+}
