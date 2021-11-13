5e585490bb411eaf90a74e398a30d084bb948026
Eli Pagan <mbk8689@gmail.com>
2021-11-13 08:03:28 -0500

    adding initial html setup

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var listedCitiesEl = document.querySelector("#listed-cities");
+var citySearchEl = document.querySelector("#citysearch");
+var weatherEl = document.querySelector("#weather");
+var forcastEl = document.querySelector("forcast");
