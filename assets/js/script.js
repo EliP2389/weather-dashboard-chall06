6a145444dcf7eb79fa6b35276522cbcc3ae945a2
Eli Pagan <mbk8689@gmail.com>
2021-11-14 23:57:58 -0500

    added icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var apiKey = "cdb53ea06b8d4e2775f8db6d2e437399";
 
 var currentCity = function () {
     // use the input's "ID" from your html along with the API in order to search a specific city
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + citySearchEl.value + `&appid=` + apiKey;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + citySearchEl.value + `&units=imperial&appid=` + apiKey;
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -27,11 +27,12 @@ var currentCity = function () {
                 // gets the 5 day weather forcast for that city
                 fiveWeather()
                 saveCity()
+                loadHistory()
                 
                 
 
 
-                let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude={part}&appid=" + apiKey;
+                let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&units=imperial&appid=" + apiKey;
 
                 fetch(apiUrl).then(function (response) {
                     if (response.ok) {
@@ -66,7 +67,7 @@ var displayWeather = function (data) {
     // var iconEl = document.querySelector("#icon");
     $("#icon").attr("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`).attr("alt", data.weather[0].main);
     var tempEl = document.querySelector("#temp");
-    tempEl.textContent = " temp: " + data.main.temp;
+    tempEl.textContent = " temp: " + data.main.temp + " F ";
     var windEl = document.querySelector("#wind");
     windEl.textContent = " wind: " + data.wind.speed;
     var humidityEl = document.querySelector("#humidity");
@@ -80,7 +81,7 @@ var displayWeather = function (data) {
 
 // new API to search for the % day forecast. exact functions as "currentWeather" to get data for the 5 day forecast
 var fiveWeather = function () {
-    let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchEl.value + `&appid=` + apiKey;
+    let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchEl.value + `&units=imperial&appid=` + apiKey;
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -94,20 +95,24 @@ var fiveWeather = function () {
 
 //  function to display 5 day forecast
 var displayFiveDay = function (data) {
-    for (let index = 0; index < 5; index++) {
+    for (let i = 0; i < 5; i++) {
 
-        var forecastEl = document.querySelector("#forecast-" + (index + 1));
+        var forecastEl = document.querySelector("#forecast-" + (i + 1));
 
         var dayTime = document.createElement("p");
-        dayTime.textContent = data.list[7 + (index * 8)].dt_txt
+        dayTime.textContent = data.list[7 + (i * 8)].dt_txt
         forecastEl.append(dayTime);
 
+        var icon = document.createElement("img");
+        icon.setAttribute("src", `http://openweathermap.org/img/wn/` + data.list[7 +(i * 8)].weather[0].icon + `@2x.png`)
+        forecastEl.append(icon);
+
         var tempEl = document.createElement("p");
-        tempEl.textContent = data.list[7 + (index * 8)].main.temp
+        tempEl.textContent = data.list[7 + (i * 8)].main.temp
         forecastEl.append(tempEl);
 
         var windEl = document.createElement("p");
-        windEl.textContent = data.list[7 + (index * 8)].wind.speed
+        windEl.textContent = data.list[7 + (i * 8)].wind.speed
         forecastEl.append(windEl);
 
     }
@@ -119,8 +124,11 @@ function saveCity(city){
 }
 
 function loadHistory() {
-    var history = localStorage.getItem("searched-city");
+    var history = localStorage.getItem("searchHistory");
     history = JSON.parse(history);
+    for(i in history){
+        searchHistory.push(history[i]);
+    }
   };
 
   
